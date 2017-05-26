86aaab60c0ac63254508167846352373043e6995
Vladimir Kamuz <v.kamuz@gmail.com>
2017-05-26 17:54:57 +0300

    Check active class for item of menu - MODX Revolution / Wayfinder

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+$(document).ready(function() {
+    if ($('.main-menu .parent.active').length == 0) { $('.main-menu .parent:first >.trigger').click(); } });
+$(document).on('click touchend', '.trigger a', function(e) { document.location = $(this).prop('href');
+    return false;
+});
+
 $(document).on('click', '.trigger', function(e) {
     $(this).parent().siblings().find('ul').slideUp();
     $(this).parent().siblings().find('.glyphicon-minus').removeClass('glyphicon-minus').addClass('glyphicon-plus');
