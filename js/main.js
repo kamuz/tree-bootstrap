a084da50438d8803c435e27668c45f9d361562bb
Vladimir Kamuz <v.kamuz@gmail.com>
2017-05-26 16:09:39 +0300

    Init

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).on('click', '.trigger', function(e) {
+    $(this).parent().siblings().find('ul').slideUp();
+    $(this).parent().siblings().find('.glyphicon-minus').removeClass('glyphicon-minus').addClass('glyphicon-plus');
+    $(this).next('ul').slideToggle();
+    var icon = $(this).children('.glyphicon');
+    if (icon.hasClass('glyphicon-minus')) {
+        icon.removeClass('glyphicon-minus').addClass('glyphicon-plus');
+    } else {
+        icon.removeClass('glyphicon-plus').addClass('glyphicon-minus');
+    }
+});
