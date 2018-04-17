8f564df1b2502194c8f8ff1e7bed5fcfc329d3e9
gayrcooper <garycooper@126.com>
2018-04-17 11:08:28 +0800

    modify function
    1 根据自己的需求改进了tree-bootstrap的功能。
    
    a.原树状菜单，当点击了很多节点后，不能保持点击节点的状态。现注释掉了该功能。我的需求是保留点击节点的状态。
    
    b.原tree-bootstrap没有右侧内容的局部刷新功能，现加上该功能。
    
    c.给节点和图标换上了测试醒目的颜色。

--- js/main.js
+++ js/main.js
@@ -1,12 +1,41 @@
 $(document).ready(function() {
+	/*
+	 * 这段代码好像没什么用处
     if ($('.main-menu .parent.active > .trigger > .glyphicon').hasClass('glyphicon-plus')) {
         $('.main-menu .parent.active > .trigger > .glyphicon').removeClass('glyphicon-plus').addClass('glyphicon-minus');
-    }
+    };
+    */
+   
+   /**
+    * 如果使用下面的代码点击完，左边的树会回到原来初始的样子，所以注释掉
+    
+    $('li a').click(function(){
+    		var url = this.href;
+    		$('#main').remove();
+    		
+    		$('#main').load(url + ' #main').fadeIn('slow');
+    })
+    */
+   
+   $("#u1").click(function(){
+   		$("#main").load("content1.html");
+   });
+   
+   $("#u2").click(function(){
+   		$("#main").load("content2.html");
+   });
+
+   $("#u3").click(function(){
+   		$("#main").load("content3.html");
+   });
 });
 
 $(document).on('click', '.trigger', function(e) {
+	
+	/* 同级的ul隐藏 
     $(this).parent().siblings().find('ul').slideUp();
     $(this).parent().siblings().find('.glyphicon-minus').removeClass('glyphicon-minus').addClass('glyphicon-plus');
+    */
     $(this).next('ul').slideToggle();
     var icon = $(this).children('.glyphicon');
     if (icon.hasClass('glyphicon-minus')) {
@@ -15,3 +44,5 @@ $(document).on('click', '.trigger', function(e) {
         icon.removeClass('glyphicon-plus').addClass('glyphicon-minus');
     }
 });
+
+
